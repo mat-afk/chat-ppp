71bbd3132ff23504a6874639517632867c11dc1a
mat-afk <mateusc.ramos@outlook.com>
2025-11-17 17:41:07 -0300

    fix: update nuxt config for prisma/client

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "nuxt-auth-utils", "@prisma/nuxt"],
   css: ["~/assets/css/main.css"],
+  vite: {
+    ssr: {
+      noExternal: ["@prisma/client"],
+    },
+  },
 });
