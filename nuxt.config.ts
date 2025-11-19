27544e89dcef7f91e7bc8d6af175332aae15eed6
mat-afk <mateusc.ramos@outlook.com>
2025-11-19 18:00:06 -0300

    feat: add websocket example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "nuxt-auth-utils"],
+  modules: ["@nuxt/ui", "nuxt-auth-utils", "@vueuse/nuxt"],
   css: ["~/assets/css/main.css"],
+  nitro: {
+    experimental: {
+      websocket: true,
+    },
+  },
 });
